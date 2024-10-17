6a86872958c86c8ea035977df6a3ec613870a4a6
hadzovicm <mirza.hadzovic@stu.ssst.edu.ba>
2024-10-17 13:38:51 +0200

    Added the USGS API showing earthquakes on the approximate territory of Bosnia and Herzegovina in the last 30 months. Added a list showing earthquakes on the left side of the homepage. Added a Leaflet map API centered on BiH, and combined it with the earthquake list to show earthquakes on the map.

--- index.js
+++ index.js
@@ -5,10 +5,22 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render("index.ejs")
-})
+
+app.get('/', async (req, res) => {
+    try {
+      const response = await axios.get('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson');
+      const bosniaEarthquakes = response.data.features.filter(eq => {
+        const [longitude, latitude] = eq.geometry.coordinates;
+        return latitude >= 42 && latitude <= 45 && longitude >= 15 && longitude <= 19;
+      });
+      res.render('index', { earthquakes: bosniaEarthquakes });
+    } catch (error) {
+      console.error('Error fetching data from USGS API:', error);
+      res.status(500).send('Error retrieving earthquake data');
+    }
+  });
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
